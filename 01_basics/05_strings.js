0ab4ab96a80cfd8e216a15bd42ef8191829375b2
suraj raghuvanshi <142202030+surajraghuwanshi05@users.noreply.github.com>
2024-06-10 07:13:22 +0000

    string

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,32 @@
+const name = "suraj"
+const repoCount = 50
+
+// console.log(name + repoCount + "Value");
+
+// console.log(`hello my name is ${name} and my repocount is ${repoCount}`);
+
+// another way to declare string it gives some extra features
+
+const gameName = new String("Suraj@123")
+
+// console.log(gameName[0]) // S 
+
+// console.log(gameName.__proto__);
+
+// console.log(gameName.length)
+// console.log(gameName.toUpperCase())
+// console.log(gameName.charAt(2))
+// console.log(gameName.indexOf('j'));
+
+const newString = gameName.substring(0, 4) // isme negetive value nahi de sakte 
+console.log(newString);
+
+const anotherString = gameName.slice(-9, 4)
+console.log(anotherString)
+
+
+
+
+const newStringOne = "   hitesh  "
+console.log(newStringOne);
+console.log(newStringOne.trim());
---
+++ 01_basics/datatyps_summary.js
@@ -0,0 +1,40 @@
+ // Primitive  
+ 
+
+  // 7 types ; String, Number, Boolean, null undefined, symbol, BigInt
+
+  const score = 20
+  const scoreValue = 200.3
+
+  const isLoggedIn = false
+  const outsideTemp = null
+
+  const id = Symbol("123")
+  // dono ko different consider karega kyuki Symbol ke andar diya hai
+  const anotherId = Symbol("123")
+
+
+  // Reference (Non primitive)
+
+  // Array, Objects, Functions
+
+
+  heros = ["shaktiman", "naagraj","doga"]
+
+
+  let myObj = {
+    name: "suraj",
+    age: 24,
+
+  }
+
+
+const myFunction = function(){
+    console.log("hello world")
+}
+
+
+
+//++++++++++++++++++++++++++++++++++
+
+// Stack(Primitive), Heap(Non-Primitive) 
