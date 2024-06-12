6557261782e78364a2dd1a48d6c6ca07aea68378
suraj raghuvanshi <142202030+surajraghuwanshi05@users.noreply.github.com>
2024-06-12 08:29:07 +0000

    flow

---
+++ 04_control_flow/one.js
@@ -0,0 +1,21 @@
+// if 
+
+
+if (true) {
+
+
+}else{
+
+}
+
+// < >, <=,>=, !=, ==, === !++
+
+// aise v kr skkte pr ek line me hi likh sakte aise me
+// const balance = 1000
+// if (balance >500) console.log("test")
+
+
+const 
+
+
+if ()
