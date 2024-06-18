3324ca1862c51a908ff7d93308b929ade7e1aad5
suraj raghuvanshi <142202030+surajraghuwanshi05@users.noreply.github.com>
2024-06-18 06:28:23 +0000

    object_getter

--- 10_classes_and_oop/object_get_set.js
+++ 10_classes_and_oop/object_get_set.js
@@ -1,3 +1,18 @@
 const User = {
-    
+    _email: 'h@afg.com',
+    _password: "abc",
+
+
+
+    get email(){
+        return this._email.toUpperCase()
+
+    },
+
+    set email(value){
+        this._email = value
+    }
 }
+
+const tea = Object.create(User)
+console.log(tea.email);
